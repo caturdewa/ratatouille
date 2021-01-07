2674b9c3050961f5aaf2ab6239c76b6067558ac6
caturdewa <acatudewa@gmail.com>
2021-01-07 20:36:56 +0700

    Cart Detail
    Cart Detail using CardView tapi masih ada error ama kesalahan beberapa

---
+++ app/src/main/java/com/example/ratatouille/CartDetails.java
@@ -0,0 +1,13 @@
+package com.example.ratatouille;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CartDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_details);
+    }
+}
